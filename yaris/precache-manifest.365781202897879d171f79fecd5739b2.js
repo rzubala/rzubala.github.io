99818b42f5d663695613a54a37faf2403020ddb2
Rafał Zubala <rzubala@operatus.org>
2019-01-14 21:43:00 +0100

    yaris

--- yaris/precache-manifest.365781202897879d171f79fecd5739b2.js
+++ yaris/precache-manifest.365781202897879d171f79fecd5739b2.js
@@ -12,23 +12,23 @@ self.__precacheManifest = [
     "url": "/yaris/static/js/runtime~main.543e09b2.js"
   },
   {
-    "revision": "2b8186b7d80edeac86ba",
-    "url": "/yaris/static/js/main.2b8186b7.chunk.js"
+    "revision": "a4e7d98072e159054e4f",
+    "url": "/yaris/static/js/main.a4e7d980.chunk.js"
   },
   {
     "revision": "2bbf94bbb258d128a5d2",
     "url": "/yaris/static/js/1.2bbf94bb.chunk.js"
   },
   {
-    "revision": "2b8186b7d80edeac86ba",
-    "url": "/yaris/static/css/main.9a731b90.chunk.css"
+    "revision": "a4e7d98072e159054e4f",
+    "url": "/yaris/static/css/main.4377d3fe.chunk.css"
   },
   {
     "revision": "2bbf94bbb258d128a5d2",
     "url": "/yaris/static/css/1.b482d9ad.chunk.css"
   },
   {
-    "revision": "cd766672b3c16f1ba65130e84c2fb1f9",
+    "revision": "91ca875fa0783fc5c2366f0dd79732a0",
     "url": "/yaris/index.html"
   }
 ];
